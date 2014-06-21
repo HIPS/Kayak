7e7d97fe5492ffea779226bc101e267c655c3d9d
Ryan Adams <rpa@seas.harvard.edu>
2014-06-21 14:40:48 -0400

    Starting from scratch in this branch.

--- kayak/test/kayak_test.py
+++ kayak/test/kayak_test.py
@@ -4,13 +4,4 @@ import numpy.random as npr
 import kayak
 
 def test_1():
-
-    N  = 10;
-    Xc = npr.randn(N)
-    X  = kayak.constants(Xc)
-    Y  = kayak.mat_sum( X )
-
-    print Y.gradient(X).value()
-
-
-    assert 'b' == 'b'
+    pass
