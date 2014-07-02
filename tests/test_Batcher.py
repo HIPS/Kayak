f648102c36b1ac1fccb311f1f97d6f63a6df5cd9
Ryan Adams <rpa@seas.harvard.edu>
2014-07-02 10:53:28 -0400

    Some tests for the batcher.

--- tests/test_Batcher.py
+++ tests/test_Batcher.py
@@ -5,3 +5,64 @@ import kayak
 
 from . import *
 
+def test_indices_1():
+    """Test with deterministic indices."""
+
+    for num_data in [1, 10, 100, 1000, 10000, 100000, 1000000]:
+        for batch_size in [1, 10, 11, 25, 50, 101, 500, 1000, 1011]:
+            
+            data_used = np.zeros((num_data,), dtype=bool)
+            batcher = kayak.Batcher(batch_size, num_data)
+            for batch in batcher:
+                data_used[batch] = True
+            
+            assert np.all(data_used)
+
+def test_indices_2():
+    """Test with random seed."""
+    npr.seed(1)
+
+    for num_data in [1, 10, 100, 1000, 10000, 100000, 1000000]:
+        for batch_size in [1, 10, 11, 25, 50, 101, 500, 1000, 1011]:
+            
+            data_used = np.zeros((num_data,), dtype=bool)
+            batcher = kayak.Batcher(batch_size, num_data, rng=npr.randint(1,100000))
+            for batch in batcher:
+                data_used[batch] = True
+            
+            assert np.all(data_used)
+
+def test_indices_3():
+    """Test with RandomState object."""
+    npr.seed(2)
+
+    for num_data in [1, 10, 100, 1000, 10000, 100000, 1000000]:
+        for batch_size in [1, 10, 11, 25, 50, 101, 500, 1000, 1011]:
+            
+            data_used = np.zeros((num_data,), dtype=bool)
+            batcher = kayak.Batcher(batch_size, num_data, rng=npr.RandomState(npr.randint(1,100000)))
+            for batch in batcher:
+                data_used[batch] = True
+            
+            assert np.all(data_used)
+
+def test_reset():
+    """Test resetting."""
+
+    for num_data in [1000, 10000, 100000, 1000000]:
+        for batch_size in [1, 10, 11, 25, 50, 101, 500]:
+            
+            batcher = kayak.Batcher(batch_size, num_data)
+
+            # Start the batcher forward.
+            batcher.next()
+
+            # Now reset.
+            batcher.reset()
+
+            # Make sure we touch all of the data.
+            data_used = np.zeros((num_data,), dtype=bool)
+            for batch in batcher:
+                data_used[batch] = True
+            
+            assert np.all(data_used)
