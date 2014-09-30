Fixed convolutional gradients

kayak/convolution.py
@@ -37,12 +37,12 @@ class Convolve1d(Differentiable):
         filtersize = self.B.shape[0]/self.ncolors
         if parent == 0:
             output     = np.zeros((self.A.shape[0], self.A.shape[1]))
-            B          = self.B.value.squeeze()
-            output = output.reshape((output.shape[0], -1, self.ncolors))
+            B          = self.B.value
+            output = output.reshape((output.shape[0], self.ncolors, -1))
             outgrad = d_out_d_self.reshape(d_out_d_self.shape[0], -1, B.shape[-1])
             for i in xrange(outgrad.shape[1]):
-                output[:,i:i+filtersize,:] += np.dot(outgrad[:,i,:], B.T).reshape((output.shape[0], filtersize, self.ncolors))
-     
+                output[:,:,i:i+filtersize] += np.dot(outgrad[:,i,:], B.T).reshape((output.shape[0], self.ncolors, filtersize)) 
+
             return output.reshape((output.shape[0], -1))
 
         elif parent == 1:

kayak/util.py
@@ -9,7 +9,7 @@ from . import EPSILON
 
 from constants import Parameter
 
-def checkgrad(variable, output, epsilon=1e-4):
+def checkgrad(variable, output, epsilon=1e-4, verbose=False):
     if not isinstance(variable, Parameter):
         raise Exception("Cannot evaluate gradient in terms of non-Parameter type %s", (type(variable)))
 
@@ -30,6 +30,10 @@ def checkgrad(variable, output, epsilon=1e-4):
         variable.value += small_array
         fd_grad[in_dims] = (fn_up - fn_dn)/epsilon
         
+        # Print finite diffs for individual parameters
+        if verbose:
+            print np.abs((an_grad[in_dims] - fd_grad[in_dims])/(fd_grad[in_dims]+EPSILON)), an_grad[in_dims], fd_grad[in_dims]
+
     return np.mean(np.abs((an_grad - fd_grad)/(fd_grad+EPSILON)))
 
 def logsumexp(X, axis=None):