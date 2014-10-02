759be2241418535e2c07c26a8ff80e7a6948d6d9
Scott Linderman <scott.linderman@gmail.com>
2014-10-02 19:30:46 -0400

    adding a few more operator mult tests

--- tests/test_OperatorMult.py
+++ tests/test_OperatorMult.py
@@ -49,6 +49,50 @@ def test_elemmult_values_3():
         assert D.shape == (5,6)
         assert np.all( close_float(D.value, np_A**2 * np_B))
 
+def test_elemmult_values_4():
+    npr.seed(1)
+
+    for ii in xrange(NUM_TRIALS):
+
+        np_A = npr.randn(5,6)
+        np_B = npr.randn(5,1)
+        A    = kayak.Parameter(np_A)
+        B    = kayak.Parameter(np_B)
+        C    = A*B
+
+        assert C.shape == np_A.shape
+        assert np.all( close_float(C.value, np_A*np_B))
+
+def test_elemmult_values_5():
+    npr.seed(2)
+
+    for ii in xrange(NUM_TRIALS):
+
+        np_A = npr.randn(5,1)
+        np_B = npr.randn(1,6)
+        np_C = npr.randn(1,1,1)
+        A    = kayak.Parameter(np_A)
+        B    = kayak.Parameter(np_B)
+        C    = kayak.Parameter(np_C)
+        D    = A*B*C
+
+        assert D.shape == (1,5,6)
+        assert np.all( close_float(D.value, np_A*np_B*np_C))
+
+def test_elemmult_values_6():
+    npr.seed(7)
+
+    for ii in xrange(NUM_TRIALS):
+
+        np_A = npr.randn(5,6)
+        np_B = npr.randn(1)
+        A    = kayak.Parameter(np_A)
+        B    = kayak.Parameter(np_B)
+        D    = A*B*A
+
+        assert D.shape == (5,6)
+        assert np.all( close_float(D.value, np_A**2 * np_B))
+
 def test_elemmult_grad_1():
     npr.seed(8)
 
